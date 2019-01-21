70aa8b1ee9980e37513b93c83b3b90c19500b6a2
Meghana_N <meghanangwd@gmail.com>
2019-01-21 18:09:22 +0530

    spring boot RESTful service example source code

---
+++ Documents/userservice/src/main/java/com/stackroute/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
+
